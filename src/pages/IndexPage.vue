878f06ebb378288a656eeabd231471fbee02e586
Dvalenciao1 <dv237968@gmail.com>
2023-08-29 22:07:29 -0500

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    const contador = ref(0);
+    console.log(contador);
+    return {
+      contador,
+    };
+  },
+});
+</script>
