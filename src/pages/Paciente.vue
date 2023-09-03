1d27a32c2de4db11b5406689cbcec44043270d2c
adchp <adchp1999@gmail.com>
2023-09-03 09:45:08 -0500

    Pantalla Paciente

---
+++ src/pages/Paciente.vue
@@ -0,0 +1,44 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="container-btn">
+    <q-btn class="derecha">Cerrar Sesion</q-btn>
+  </div>
+  <div>
+    <h3 class="derecha">Seleccione una opcion</h3>
+    <div class="row">
+      <div class="col-6">
+        <q-img
+          class="imagen"
+          src="../assets/quasar-logo-vertical.svg"
+          alt="doctor"
+        ></q-img>
+      </div>
+      <div class="col-3">
+        <q-img
+          class="imagen"
+          src="../assets/quasar-logo-vertical.svg"
+          alt="doctor"
+        ></q-img>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          numquam excepturi ab modi a esse nesciunt. Pariatur magni
+          perspiciatis, obcaecati laudantium temporibus delectus ad laboriosam
+          cum optio accusantium natus corrupti.
+        </p>
+      </div>
+      <div class="col-3">
+        <q-img
+          class="imagen"
+          src="../assets/quasar-logo-vertical.svg"
+          alt="doctor"
+        ></q-img>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          numquam excepturi ab modi a esse nesciunt. Pariatur magni
+          perspiciatis, obcaecati laudantium temporibus delectus ad laboriosam
+          cum optio accusantium natus corrupti.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
